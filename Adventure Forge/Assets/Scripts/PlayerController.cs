f2f87090619502549c757066ae66e43c4205ed77
Faldirdk <Mikkeljohansen22@gmail.com>
2016-10-29 03:40:38 +0200

    Init
    karakter kan bevæge sig

---
+++ Adventure Forge/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Vector3 direction = new Vector3(0, 0, 0);
+    Rigidbody rb;
+
+    public int speed;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.z = Input.GetAxisRaw("Vertical");
+        direction.Normalize();
+
+        rb.AddForce(direction * speed);
+
+        if (direction.x != 0 || direction.z != 0)
+        {
+            transform.rotation = Quaternion.LookRotation(direction);
+        }
+    }
+}
